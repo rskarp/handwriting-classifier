c7326047cdddd2228a585b047538dc6bdfa61446
rskarp <rkarp1997@gmail.com>
2024-02-25 18:05:49 -0500

    initial data conditioning
    Add initial data conditioning steps: convert image to grayscale, increase contrast, morphological adjustment to remove noise & sharpen edges, binarize to prepare for object extraction

--- Scripts/feature_extraction.m
+++ Scripts/feature_extraction.m
@@ -1,4 +1,4 @@
-% Feature extraction code
+% Data Conditioning & Feature extraction code
 close all;
 clear;
 
@@ -8,12 +8,42 @@ trainMeta = readmatrix('Dataset/written_name_train_v2.csv','OutputType','string'
 trainMeta = trainMeta(2:length(trainMeta),:);
 
 % Only use first 3 files for now
-first3 = trainMeta(1:3,:);
+first4 = trainMeta(1:4,:);
 
 % Iterate through training images
+showPlots=1;
 trainDataPath = 'Dataset/train_v2/train/';
-for i = 1:length(first3)
-    filename = first3(i,1);
+for i = 1:length(first4)
+    filename = first4(i,1);
+    % Read original image
     I = imread(trainDataPath+filename);
-    figure; imshow(I);
+    % Convert to bw so it can be passed to imadjust
+    Igray = rgb2gray(I);
+    % Enhance contrast using imadjust
+    Ienhanced = imadjust(Igray);
+    % Morphological opening to remove small background noises
+    se = strel('square',3);
+    Iopen = imopen(Ienhanced,se);
+    % Morphological erosion to sharpen edges
+    se = strel('square',2);
+    Ierode = imerode(Iopen,se);
+    % Binarize image so we can use regionprops
+    Ithresh = Ierode > 128;
+
+    % Plot Images
+    if showPlots
+        figure; 
+        tiledlayout(3,2,"TileSpacing","compact");
+        nexttile, imshow(I), title('Original');
+        nexttile, imshow(Igray), title('RGB converted to gray');
+        nexttile, imshow(Ienhanced), title('Contrast Enhanced');
+        nexttile, imshow(Iopen), title('Morphologically Opened');
+        nexttile, imshow(Ierode), title('Morphologically Eroded');
+        nexttile, imshow(Ithresh), title('Binary Thresholded');
+    end
+    
+    % TO DO:
+    % Constant image height
+    % Extract objects/spaces - remove hyphens, typed letters, small objects
+    % Extract letter features: regionpros, Hu moments
 end
