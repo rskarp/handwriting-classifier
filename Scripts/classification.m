9923ff2ef0d9944099320898697bebb91a13d902
rskarp <rkarp1997@gmail.com>
2024-03-10 19:07:00 -0400

    testing various classifier types
    - try using various classifiers (Euclidean, Bayesian, SVM) to support why we chose to go with neural network instead

--- Scripts/classification.m
+++ Scripts/classification.m
@@ -1,5 +1,99 @@
-% This is a placeholder for our classifier code
+% Letter Classification Code
+% Testing Various Classifier types (Euclidean, Bayesian, KNN, SVM)
+close all;
+clear;
 
-%this is a test
+% Load features data
+trainFeatures = load('Features/train_features.mat').featuresData;
+validationFeatures = load('Features/validation_features.mat').featuresData;
+testFeatures = load('Features/test_features.mat').featuresData;
 
-%This is another test -shad
+%% All possible characters
+chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+% Convert features to matrix
+[trainData, trainLabels] = convertFeaturesToMatrix(trainFeatures, chars);
+[validationData, validationLabels] = convertFeaturesToMatrix(validationFeatures, chars);
+[testData, testLabels] = convertFeaturesToMatrix(testFeatures, chars);
+
+% Calculate statistics for Euclidean & Bayesian classifiers
+[N, numFeatures] = size(trainData);
+numClasses = length(chars);
+means = zeros([numFeatures,numClasses]);
+covs = zeros([numFeatures,numFeatures,numClasses]);
+P = zeros([1, numClasses]);
+for c=1:numClasses
+    classData = trainData(trainLabels==c,:);
+    classMean = mean(classData);
+    means(:,c) = classMean';
+    covs(:,:,c) = cov(classData);
+    P(c) = length(classData)/length(trainData);
+end
+
+% Euclidean Classifier
+fprintf('\nEuclidean Classifier\n')
+% Train
+z=euclidean_classifier(means,trainData');
+euclideanError = sum(trainLabels~=z)/length(z);
+fprintf('     Training Error: %.4f \n', euclideanError)
+% Validate
+z=euclidean_classifier(means,validationData');
+euclideanError = sum(validationLabels~=z)/length(z);
+fprintf('     Validation Error: %.4f \n', euclideanError)
+
+% Bayesian Classifier
+% fprintf('\nBayesian Classifier\n')
+% % Train
+% z = bayes_classifier(means,covs,P,trainData');
+% error = sum(trainLabels~=z)/length(z);
+% fprintf('     Training Error: %.4f \n', error)
+% % Validate
+% z = bayes_classifier(means,covs,P,validationData');
+% error = sum(validationLabels~=z)/length(z);
+% fprintf('     Validation Error: %.4f \n', error)
+
+% KNN Classifier
+% fprintf('\nKNN Classifier\n')
+% % Train
+% k = length(chars);
+% z = k_nn_classifier(trainData,trainLabels,k,trainData);
+% error = sum(trainLabels~=z)/length(z);
+% fprintf('     Training Error: %.4f \n', error)
+% % Test
+% z = k_nn_classifier(TrainData,TrainLabels_positive,k,TestData);
+% error = sum(TestLabels_positive~=z)/length(z);
+% fprintf('     Testing Error: %.4f \n', error)
+
+% SVM Classifier
+
+%% Test Unknown data using trained classifier
+filenames = unique(cellfun(@(x) x.Filename, trainFeatures));
+% Generate text string for each input data file
+for i = 1:length(filenames)
+    fname = filenames(i);
+    % Get all letters in the file
+    letters = featuresData(cellfun(@(x) x.Filename==fname, trainFeatures));
+    for l = 1:length(letters)
+        % TODO: Classify each letter
+    end
+end
+
+%% Function Definitions
+
+% Convert numeric feature data to matrix & get class labels
+function [data, labels] = convertFeaturesToMatrix(features, chars)
+    NUM_FEATURES = 19; % Update this if our features change
+    data = zeros([length(features),NUM_FEATURES]);
+    labels = zeros([1,length(features)]);
+    for i = 1:length(features)
+        obj = features{i};
+        row = [obj.Area, obj.Centroid(1), obj.Centroid(2), obj.MajorAxisLength,...
+            obj.MinorAxisLength, obj.Eccentricity, obj.Orientation, obj.ConvexArea,...
+            obj.Circularity, obj.Solidity, obj.Perimeter, obj.HuMoments];
+        class = strfind(chars,obj.Letter);
+        data(i,:) = row;
+        if ~isempty(class)
+            labels(i) = class;
+        end
+    end
+end
