d695c67965992a578dbeba6c193f5169944b5413
rskarp <rkarp1997@gmail.com>
2024-03-16 23:45:02 -0400

    plot confusion matrix & classify test data
    - classify test data using the Euclidean, Bayesian, Naive Bayes, and KNN classifiers
    - plot confusion matrix for best classifier (KNN)

--- Scripts/classification.m
+++ Scripts/classification.m
@@ -16,6 +16,11 @@ chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 [validationData, validationLabels] = convertFeaturesToMatrix(validationFeatures, chars);
 [testData, testLabels] = convertFeaturesToMatrix(testFeatures, chars);
 
+% Normalize feature columns
+trainData = normc(trainData);
+validationData = normc(validationData);
+testData = normc(testData);
+
 % Calculate statistics for Euclidean & Bayesian classifiers
 [N, numFeatures] = size(trainData);
 numClasses = length(chars);
@@ -44,6 +49,10 @@ fprintf('     Training Error: %.4f \n', euclideanError)
 z=euclidean_classifier(means,validationData');
 euclideanError = sum(validationLabels~=z)/length(z);
 fprintf('     Validation Error: %.4f \n', euclideanError)
+% Test
+z=euclidean_classifier(means,testData');
+euclideanError = sum(testLabels~=z)/length(z);
+fprintf('     Test Error: %.4f \n', euclideanError)
 
 % Bayesian Classifier
 fprintf('\nBayesian Classifier\n')
@@ -55,11 +64,15 @@ fprintf('     Training Error: %.4f \n', error)
 z = bayes_classifier(means,covs,P,validationData');
 error = sum(validationLabels~=z)/length(z);
 fprintf('     Validation Error: %.4f \n', error)
+% Test
+z = bayes_classifier(means,covs,P,testData');
+error = sum(testLabels~=z)/length(z);
+fprintf('     Test Error: %.4f \n', error)
 
 % Naive Bayes Classifier
 fprintf('\nNaive Bayes Classifier\n')
 % Train
-Mdl = fitcnb(trainData,trainLabels);confusionchart(YTest,label)
+Mdl = fitcnb(trainData,trainLabels);
 z = predict(Mdl,trainData);
 error = sum(trainLabels~=z')/length(z);
 fprintf('     Training Error: %.4f \n', error)
@@ -67,19 +80,45 @@ fprintf('     Training Error: %.4f \n', error)
 z = predict(Mdl,validationData);
 error = sum(validationLabels~=z')/length(z);
 fprintf('     Validation Error: %.4f \n', error)
+% Test
+z = predict(Mdl,testData);
+error = sum(testLabels~=z')/length(z);
+fprintf('     Test Error: %.4f \n', error)
 
 % KNN Classifier
 fprintf('\nKNN Classifier\n')
 % Train
 Mdl = fitcknn(trainData,trainLabels,'NumNeighbors',10);
-z = predict(Mdl,trainData);
-error = sum(trainLabels~=z')/length(z);
+zTrain = predict(Mdl,trainData);
+error = sum(trainLabels~=zTrain')/length(zTrain);
 fprintf('     Training Error: %.4f \n', error)
 % Validate
-z = predict(Mdl,validationData);
-error = sum(validationLabels~=z')/length(z);
+zVal = predict(Mdl,validationData);
+error = sum(validationLabels~=zVal')/length(zVal);
 fprintf('     Validation Error: %.4f \n', error)
-% confusionchart(validationLabels,z)
+% Test
+zTest = predict(Mdl,testData);
+error = sum(testLabels~=zTest')/length(zTest);
+fprintf('     Test Error: %.4f \n', error)
+
+% Plot Confusion Matrices for KNN Classifier
+toCaptialLetters = @(labels) categorical(labels,1:26,num2cell(chars(1:26)));
+
+trainLabs = toCaptialLetters(trainLabels);
+valLabs = toCaptialLetters(validationLabels);
+testLabs = toCaptialLetters(testLabels);
+zzTrain = toCaptialLetters(zTrain);
+zzVal = toCaptialLetters(zVal);
+zzTest = toCaptialLetters(zTest);
+allLabs = [trainLabs, valLabs, testLabs];
+zzAll = [zzTrain; zzVal; zzTest;];
+
+figure
+sgtitle('KNN Classifier Results');
+subplot(2,2,1); confusionchart(trainLabs,zzTrain,'Title','Train');
+subplot(2,2,2); confusionchart(valLabs,zzVal,'Title','Validate');
+subplot(2,2,3); confusionchart(testLabs,zzVal,'Title','Test');
+subplot(2,2,4); confusionchart(allLabs,zzAll,'Title','Overall');
 
 %% Test Unknown data using trained classifier
 filenames = unique(cellfun(@(x) x.Filename, trainFeatures));
